8b98ec5d4343d4c53516a5e27b93ed6be9f188d2
yda770 <35491939+yda770@users.noreply.github.com>
2019-02-03 16:33:51 +0200

    Add project files.

---
+++ Yarmoolka/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yarmoolka.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
