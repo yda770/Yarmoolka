03de3a46ad1793653a3cdb33261a02bd3529fd0e
yda770 <35491939+yda770@users.noreply.github.com>
2019-02-03 19:37:27 +0200

    fix role + connection string

--- Yarmoolka/Startup.cs
+++ Yarmoolka/Startup.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Yarmoolka.Models;
+using Yarmoolka.Services;
 
 namespace Yarmoolka
 {
@@ -24,33 +23,52 @@ namespace Yarmoolka
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+
+
+            services.AddDbContext<YarmoolkaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("YarmoolkaContext")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+             .AddEntityFrameworkStores<YarmoolkaContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMemoryCache();
+
+
+            services.AddMvc();
+
+            services.AddAuthorization(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
+
         {
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
@@ -58,6 +76,11 @@ namespace Yarmoolka
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
+
+
     }
+
+
 }
